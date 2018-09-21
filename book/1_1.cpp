610370710b123ec28251ac484f9e5e943e80720c
HP-cmd <zgpio@qq.com>
2018-09-21 08:58:46 +0800

    tiny

--- book/1_1.cpp
+++ book/1_1.cpp
@@ -23,25 +23,26 @@ int main()
 #endif
     // e.g.
     // n[0] n[1] n[2] n[3]
-    //  1    2    3   4
+    //  1    2    3    4
     char n[10];  // n <= 10^9
     while (scanf("%s", n) != EOF) {
-        cout << subNumber("1234", 0, 0) << endl;
-        cout << subNumber("1234", 2, 1) << endl;
-        cout << subNumber("1234", 0, 3) << endl;
         int64 result[10] = {0};
         int i, j, k, len = strlen(n);
         int inc;
-        for (i = len - 1; i >= 0; i--) {
+        for (i = 0; i < len; i++) {
+            //  subl    k   subr
+            // (0, i-1) i (i+1, len-1)
             k = subNumber(n, i, i);
             for (j = 0; j < 10; j++) {
+                int subl = subNumber(n, 0, i - 1);
                 if (j < k)
-                    inc = (subNumber(n, 0, i - 1) + 1) * pow(10, len - i - 1);
-                else if (j == k)
-                    inc = (subNumber(n, 0, i - 1)) * pow(10, len - i - 1) +
-                          (subNumber(n, i + 1, len - 1) + 1);
+                    inc = (subl + 1) * pow(10, len - i - 1);
+                else if (j == k) {
+                    int subr = subNumber(n, i + 1, len - 1);
+                    inc = subl * pow(10, len - i - 1) + (subr + 1);
+                }
                 else
-                    inc = (subNumber(n, 0, i - 1)) * pow(10, len - i - 1);
+                    inc = subl * pow(10, len - i - 1);
                 result[j] += inc;
             }
         }
@@ -50,7 +51,7 @@ int main()
         }
         result[0] -= len;
         for (i = 0; i < 10; i++) {
-            printf("%I64d ", result[i]);
+            printf("%lld ", result[i]);  // I64d
         }
         printf("\n");
     }
