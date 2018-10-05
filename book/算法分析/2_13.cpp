0587b996cb33ff3465ddf5b302b4a6714e585e08
TP-cmd <zgpio@qq.com>
2018-10-05 23:45:06 +0800

    tiny

---
+++ book/算法分析/2_13.cpp
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <string.h>
+#include <algorithm>
+#include <cmath>
+#include <iostream>
+using namespace std;
+
+template <typename T>
+void mergesort(T *a, int left, int right)
+{
+    if(left<right)
+    {
+        int j=(int)sqrt(right-left+1);
+        if(j>1)
+        {
+            for(int i=0;i<j;i++)
+                mergesort(a, left+i*j, left+(i+1)*j-1);
+            mergesort(a, left+j*j, right);
+        }
+        mergeall(a, left, right);
+    }
+}
+
+int main()
+{
+    int a[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    int find = 9;
+    int i, j;
+    mergesort(a, find, 0, 9, i, j);
+    std::cout << i << j << std::endl;
+
+    return 0;
+}
