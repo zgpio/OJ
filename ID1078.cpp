23508ba132be1e54fee2ef3626270fa49c98a504
HP-cmd <zgpio@qq.com>
2018-10-31 16:35:25 +0800

    tiny

--- ID1078.cpp
+++ ID1078.cpp
@@ -37,6 +37,7 @@ int main()
 #endif
     int k;
     while (cin >> n >> k && !(n == -1 && k == -1)) {
+        ans = 0;
         for (int i = 0; i < n; ++i)
             for (int j = 0; j < n; ++j) cin >> cheese[i][j];
         // for (int i = 0; i < n; ++i) {
