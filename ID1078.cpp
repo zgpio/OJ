d2d3589b2607e7ccb67b4a111c5e0e2d80e4033e
TP-cmd <zgpio@qq.com>
2018-11-06 23:10:51 +0800

    ac 1078

--- ID1078.cpp
+++ ID1078.cpp
@@ -6,28 +6,29 @@
 #include <iostream>  //其中包含了stl_algobase
 #include <list>
 using namespace std;
-const int N = 105;
-int cheese[N][N];
-int mov[4][2] = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};  // 上下左右
-int n;
-int ans = 0;
+const int M = 102;
+// int dir[4][2] = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};  // 上下左右
+int dir[4][2] = {0, 1, 1, 0, 0, -1, -1, 0};
+int n, k, mat[M][M], dp[M][M];
 
-void dfs(int need, int x, int y, int eatd)
+bool check(int x, int y)
 {
-    for (int i = 0; i < 4; ++i) {  // 四个方向试探
-        int nx = x + mov[i][0], ny = y + mov[i][1];
-        if (!(0 <= nx && nx < n) || !(0 <= ny && ny < n)) continue;
-        if (cheese[nx][ny] == 0) continue;
-        int next = cheese[nx][ny];
-        if (next > need) {
-            eatd += next;
-            cheese[nx][ny] = 0;
-            if (ans < eatd) ans = eatd;
-            dfs(next, nx, ny, eatd);
-            eatd -= next;
-            cheese[nx][ny] = next;
+    if (x < 0 || y < 0 || x >= n || y >= n) return false;
+    return true;
+}
+
+int dfs(int x, int y)
+{
+    if (dp[x][y] != -1) return dp[x][y];
+    int sum = 0;
+    for (int p = 0; p < 4; ++p)
+        for (int i = 1; i <= k; ++i) {
+            int nx = x + i * dir[p][0];  //方向
+            int ny = y + i * dir[p][1];
+            if (check(nx, ny) && mat[x][y] < mat[nx][ny])
+                sum = max(sum, dfs(nx, ny));  //找到相邻四个方向到此点的最优
         }
-    }
+    return dp[x][y] = sum + mat[x][y];
 }
 
 int main()
@@ -35,20 +36,20 @@ int main()
 #ifndef ONLINE_JUDGE
     freopen("./input/1078", "rt", stdin);
 #endif
-    int k;
     while (cin >> n >> k && !(n == -1 && k == -1)) {
-        ans = 0;
         for (int i = 0; i < n; ++i)
-            for (int j = 0; j < n; ++j) cin >> cheese[i][j];
-        for (int i = 0; i < n; ++i) {
             for (int j = 0; j < n; ++j) {
-                cout << cheese[i][j] << " ";
+                cin >> mat[i][j];
             }
-            std::cout << std::endl;
-        }
-        dfs(0, 0, 0, cheese[0][0]);
-        std::cout << ans << std::endl;
+        // for (int i = 0; i < n; ++i) {
+        //     for (int j = 0; j < n; ++j) {
+        //         cout << cheese[i][j] << " ";
+        //     }
+        //     std::cout << std::endl;
+        // }
+        memset(dp, -1, sizeof(dp));
+        dfs(0, 0);
+        cout << dp[0][0] << endl;
     }
-
     return 0;
 }
