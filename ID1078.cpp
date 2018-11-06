579e3c3b1f7c35c6f440ec7e21eeb10f26b4909f
HP-cmd <zgpio@qq.com>
2018-11-06 22:38:01 +0800

    tiny

--- ID1078.cpp
+++ ID1078.cpp
@@ -11,25 +11,25 @@ int cheese[N][N];
 int mov[4][2] = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};  // 上下左右
 int n;
 int ans = 0;
-void dfs(int need, int x, int y, int eat)
+
+void dfs(int need, int x, int y, int eatd)
 {
     for (int i = 0; i < 4; ++i) {  // 四个方向试探
-        int tx = x, ty = y;
-        tx += mov[i][0];
-        ty += mov[i][1];
-        if (!(0 <= tx && tx < n) || !(0 <= ty && ty < n)) continue;
-        if (cheese[tx][ty] == 0) continue;
-        int save = cheese[tx][ty];
-        if (save > need) {
-            eat += save;
-            if (ans < eat) ans = eat;
-            cheese[tx][ty] = 0;
-            dfs(save, tx, ty, eat);
-            eat -= save;
-            cheese[tx][ty] = save;
+        int nx = x + mov[i][0], ny = y + mov[i][1];
+        if (!(0 <= nx && nx < n) || !(0 <= ny && ny < n)) continue;
+        if (cheese[nx][ny] == 0) continue;
+        int next = cheese[nx][ny];
+        if (next > need) {
+            eatd += next;
+            cheese[nx][ny] = 0;
+            if (ans < eatd) ans = eatd;
+            dfs(next, nx, ny, eatd);
+            eatd -= next;
+            cheese[nx][ny] = next;
         }
     }
 }
+
 int main()
 {
 #ifndef ONLINE_JUDGE
@@ -40,12 +40,12 @@ int main()
         ans = 0;
         for (int i = 0; i < n; ++i)
             for (int j = 0; j < n; ++j) cin >> cheese[i][j];
-        // for (int i = 0; i < n; ++i) {
-        //     for (int j = 0; j < n; ++j) {
-        //         cout << cheese[i][j] << " ";
-        //     }
-        //     std::cout << std::endl;
-        // }
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < n; ++j) {
+                cout << cheese[i][j] << " ";
+            }
+            std::cout << std::endl;
+        }
         dfs(0, 0, 0, cheese[0][0]);
         std::cout << ans << std::endl;
     }
