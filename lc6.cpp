c87eb84fa1625a7e84dd05a91542b3821a90d951
HP-cmd <zgpio@qq.com>
2018-12-09 21:19:36 +0800

    ac lc6

---
+++ lc6.cpp
@@ -0,0 +1,58 @@
+#include <assert.h>
+#include <cstdio>
+#include <deque>
+#include <fstream>
+#include <iostream>
+#include <list>
+#include <map>
+#include <queue>
+#include <string>
+#include <tuple>
+#include <unordered_map>
+using namespace std;
+
+// 6. ZigZag Conversion
+class Solution {
+public:
+    string convert(string s, int numRows)
+    {
+        string ret;
+        map<int, list<char>> dat;
+        int dir = 0, i = 0, cnt = 0;
+        while (s[i]) {
+            if (cnt == 0 || cnt == numRows - 1) dir = !dir;
+            auto ser = dat.find(cnt);
+            if (ser != dat.end()) {
+                dat[cnt].push_back(s[i]);
+            }
+            else {
+                dat[cnt] = list<char>{s[i]};
+            }
+            if (numRows != 1) {
+                if (dir)
+                    ++cnt;
+                else
+                    --cnt;
+            }
+            ++i;
+        }
+        for (int i = 0; i < numRows; ++i) {
+            if (dat.find(i) != dat.end()) {
+                for (auto it = dat[i].begin(); it != dat[i].end(); ++it) {
+                    ret.append(string{*it});
+                }
+            }
+        }
+        return ret;
+    }
+};
+
+int main()
+{
+    Solution sol;
+    assert(sol.convert("PAYPALISHIRING", 3) == "PAHNAPLSIIGYIR");
+    assert(sol.convert("PAYPALISHIRING", 4) == "PINALSIGYAHRPI");
+    assert(sol.convert("AB", 1) == "AB");
+    std::cout << "end" << std::endl;
+    return 0;
+}
