ba682847d1eef8c1810e7b76e73fc21425a06fe8
TP-manjaro <zgpio@qq.com>
2019-05-29 07:40:34 +0800

    ac lc17

---
+++ lc17_Letter-Combinations-of-a-Phone-Number.cpp
@@ -0,0 +1,66 @@
+#include <assert.h>
+#include <algorithm>
+#include <climits>
+#include <cstdio>
+#include <cstring>
+#include <deque>
+#include <fstream>
+#include <iostream>
+#include <limits>
+#include <list>
+#include <map>
+#include <queue>
+#include <string>
+#include <tuple>
+#include <unordered_map>
+using namespace std;
+
+vector<vector<string>> keys = {
+    {"a", "b", "c"}, {"d", "e", "f"},      {"g", "h", "i"},
+    {"j", "k", "l"}, {"m", "n", "o"},      {"p", "q", "r", "s"},
+    {"t", "u", "v"}, {"w", "x", "y", "z"},
+};
+class Solution {  // Runtime: 0 ms, faster than 100.00% of C++ online submissions for Letter Combinations of a Phone Number.
+public:
+    vector<string> letterCombinations(string digits)
+    {
+        queue<string> ret;
+        for (auto i : digits) {
+            vector<string> t = keys[i - '2'];
+
+            if (ret.empty()) {
+                for (auto i : t) ret.push(i);
+            }
+            else {
+                int sz = ret.size();
+                while (sz--) {
+                    string top = ret.front();
+                    ret.pop();
+                    for (auto k : t) {
+                        string t = top;
+                        t += k;
+                        ret.push(t);
+                    }
+                }
+            }
+        }
+
+        vector<string> ans;
+        while (!ret.empty()) {
+            ans.push_back(ret.front());
+            // std::cout << ret.front() << std::endl;
+            ret.pop();
+        }
+        return ans;
+    }
+};
+
+int main()
+{
+    Solution sol;
+
+    vector<string> a1{"ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"};
+    assert(sol.letterCombinations("23") == a1);
+
+    return 0;
+}
