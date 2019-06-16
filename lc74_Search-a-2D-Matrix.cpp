4a3de8d2a1bc8883dab30fdc6fc76fbda4cbf6cf
TP-manjaro <zgpio@qq.com>
2019-06-17 07:59:29 +0800

    ac lc74

---
+++ lc74_Search-a-2D-Matrix.cpp
@@ -0,0 +1,49 @@
+#include <assert.h>
+#include <algorithm>
+#include <climits>
+#include <cstdio>
+#include <cstring>
+#include <deque>
+#include <fstream>
+#include <iostream>
+#include <limits>
+#include <list>
+#include <map>
+#include <queue>
+#include <string>
+#include <tuple>
+#include <unordered_map>
+using namespace std;
+
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target)
+    {
+        if (matrix.size() == 0) return false;
+        if (matrix[0].size() == 0) return false;
+        for (auto row : matrix) {
+            if (std::binary_search(row.begin(), row.end(), target))
+                return true;
+            else if (row[0] < target && target < row.back())
+                return false;
+        }
+        return false;
+    }
+};
+
+int main()
+{
+    Solution sol;
+    vector<vector<int>> matrix = {
+        {1, 3, 5, 7}, {10, 11, 16, 20}, {23, 30, 34, 50}};
+    vector<vector<int>> m2 = {{}};
+    vector<vector<int>> m3 = {};
+    int target = 3;
+    int target2 = 4;
+
+    assert(sol.searchMatrix(matrix, target));
+    assert(!sol.searchMatrix(matrix, target2));
+    assert(!sol.searchMatrix(m2, target));
+    assert(!sol.searchMatrix(m3, target));
+    return 0;
+}
