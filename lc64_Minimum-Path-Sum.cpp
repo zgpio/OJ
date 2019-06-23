d9c2eddd65955a56f3d7b91e0b759d5f26939447
zgp <zgp@Apple.local>
2019-06-23 09:00:11 +0800

    todo: lc64

---
+++ lc64_Minimum-Path-Sum.cpp
@@ -0,0 +1,38 @@
+#include <algorithm>
+#include <assert.h>
+#include <climits>
+#include <cstdio>
+#include <cstring>
+#include <deque>
+#include <fstream>
+#include <iostream>
+#include <limits>
+#include <list>
+#include <map>
+#include <queue>
+#include <string>
+#include <tuple>
+#include <unordered_map>
+using namespace std;
+
+class Solution {
+public:
+    int minPathSum(vector<vector<int>> &grid) {
+
+    }
+private:
+    int fun(vector<vector<int>> &grid, int x, int y)
+    {
+
+    }
+};
+
+int main()
+{
+    Solution sol;
+
+    vector<vector<int>> t1 = {{1, 3, 1}, {1, 5, 1}, {4, 2, 1}};
+    assert(sol.minPathSum(t1) == 7);
+
+    return 0;
+}
