afcb40b2c30a97612b7ab4b534c188968e9860ed
zgp <zgp@Hack.local>
2019-07-10 22:04:38 +0800

    tiny

--- lc63_Unique-Paths-II.cpp
+++ lc63_Unique-Paths-II.cpp
@@ -3,15 +3,26 @@ using namespace std;
 
 class Solution {
     int ans;
+    int m, n;
 public:
     int uniquePathsWithObstacles(vector<vector<int>> &obstacleGrid)
     {
+        ans = 0;
         int m = obstacleGrid.size();
         int n = obstacleGrid[0].size();
+        dfs(obstacleGrid, 0, 0);
+        return ans;
     }
-    void dfs(int x, int y, int )
+    void dfs(vector<vector<int>> &a, int x, int y)
     {
-        if(x)
+        if(x==m-1 && y ==n-1)
+        {
+            ans++;
+        }
+        if(x+1<m && a[x+1][y]==0)
+            dfs(a, x+1, y);
+        if(y+1<n && a[x][y+1]==0)
+            dfs(a, x, y+1);
     }
 };
 
