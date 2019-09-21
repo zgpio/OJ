edc49af25718d6cf48f4ae1ae705804544d015c9
Hack <zgpio@qq.com>
2019-09-21 16:22:12 +0800

    ac

---
+++ lc402_remove-k-digits.py
@@ -0,0 +1,53 @@
+# from itertools import combinations
+from typing import List
+
+
+# NOTE:
+# The length of num is less than 10002 and will be ≥ k.
+# The given num does not contain any leading zero.
+class Solution:
+    def removeKdigits(self, num: str, k: int) -> str:
+        n = len(num)
+        if n == k:
+            return '0'
+        # NOTE: There is no stack in the python standard library.
+        s = []
+        for i, c in enumerate(num):
+
+            # 尽可能地移除比c大的栈顶元素
+            while len(s) > 0 and c < s[-1] and i - len(s) < k:
+                del s[-1]
+            s.append(c)
+
+        print(str(int(''.join(s[:n - k]))))
+        return str(int(''.join(s[:n - k])))
+
+
+def main():
+
+    sol = Solution()
+    num = "1432219"
+    k = 3
+    assert sol.removeKdigits(num, k) == "1219"
+
+    num = "10200"
+    k = 1
+    assert sol.removeKdigits(num, k) == "200"
+
+    num = "10"
+    k = 2
+    assert sol.removeKdigits(num, k) == "0"
+
+    num = "1234560"
+    k = 6
+    assert sol.removeKdigits(num, k) == "0"
+    num = "9"
+    k = 1
+    assert sol.removeKdigits(num, k) == "0"
+    num = "112"
+    k = 1
+    assert sol.removeKdigits(num, k) == "11"
+
+
+if __name__ == "__main__":
+    main()
