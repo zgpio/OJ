bb72ca80d1f49a58931c23be23c916fd77a54310
Hack <zgpio@qq.com>
2019-11-21 09:37:31 +0800

    tiny

--- boost/pipe_eg00.cpp
+++ boost/pipe_eg00.cpp
@@ -1,18 +1,21 @@
-#include <boost/thread.hpp>
 #include <boost/asio.hpp>
-#include <cstdio>
 #include <boost/process/async_pipe.hpp>
+#include <boost/thread.hpp>
+#include <cstdio>
 namespace bp = boost::process;
 
-
+/// echo hello, pipe > \\\\.\\pipe\\SamplePipe
 int main()
 {
     boost::asio::io_service ios;
-    std::vector<char> buf(20);
+    std::vector<char> buf(5);
     bp::async_pipe ap(ios, "\\\\.\\pipe\\SamplePipe");
-    boost::asio::async_read(ap, boost::asio::buffer(buf),
-            [](const boost::system::error_code &ec, std::size_t size) {});
+    boost::asio::async_read(
+        ap, boost::asio::buffer(buf),
+        [](const boost::system::error_code &ec, std::size_t size) {});
     ios.run();
+    for (auto i : buf)
+        printf("%c", i);
 
     return 0;
 }
