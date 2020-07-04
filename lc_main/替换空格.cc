2c45e222e8f1e8934aa5be6cc6fbd5dc3fe5e437
Hack <zgpio@qq.com>
2020-07-04 09:43:56 +0800

    替换空格

---
+++ lc_main/替换空格.cc
@@ -0,0 +1,42 @@
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+    void replaceSpace(char *str, int length)
+    {
+        int cnt = 0; // 统计空格数
+        for (int i = 0; i < length; i++) {
+            if (str[i] == ' ') {
+                cnt++;
+            }
+        }
+
+        int len = length + cnt * 2;
+        for (int i = length - 1, j = len - 1; i >= 0 && j >= 0;) {
+            if (str[i] == ' ') {
+                str[j--] = '0';
+                str[j--] = '2';
+                str[j--] = '%';
+                i--;
+            }
+            else {
+                str[j--] = str[i--];
+            }
+        }
+        str[len] = '\0';
+    }
+};
+
+int main()
+{
+    char str[4 + 3 * 3 + 1] = "a b c d";
+
+    Solution solu;
+    solu.replaceSpace(str, strlen(str));
+
+    cout << str << endl;
+
+    return 0;
+}
