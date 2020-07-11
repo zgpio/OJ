e4a6384146e62279068e991716f825ffc844483e
Hack <zgpio@qq.com>
2020-07-11 17:54:31 +0800

    lc98

---
+++ lc_main/lc98_验证二叉搜索树.cc
@@ -0,0 +1,45 @@
+#include "lc/bt.h"
+#include <iostream>
+#include <stack>
+using namespace std;
+
+// 中序遍历二叉搜索树可得到一个关键字的有序序列, 由小到大排序.
+// 用自定义栈模拟深度优先遍历, 先序、中序、后序遍历都属于深度优先.
+class Solution {
+public:
+    bool isValidBST(TreeNode *root)
+    {
+        stack<TreeNode *> stack;
+        long long pre = LLONG_MIN;
+
+        while (!stack.empty() || root != nullptr) {
+            while (root != nullptr) {
+                stack.push(root);
+                root = root->left;
+            }
+            root = stack.top();
+            stack.pop();
+            // 如果中序遍历访问的节点的值小于等于前一个元素, 说明不是二叉搜索树
+            if (root->val <= pre) return false;
+            pre = root->val;
+            root = root->right;
+        }
+        return true;
+    }
+};
+
+int main(int argc, char *argv[])
+{
+    vector<pair<initializer_list<int>, bool>> cases = {
+        {{1, 2, 3, 4, 5, 6}, false},
+        {{5, 1, 4, -1, -1, 3, 6}, false},
+        {{1, 1}, false},
+        {{1, -1, 1}, false},
+        {{10, 5, 15, -1, -1, 6, 20}, false}};
+    Solution s;
+    for (auto i : cases) {
+        TreeNode *t = constructT(i.first);
+        assert(s.isValidBST(t) == i.second);
+    }
+    return 0;
+}
