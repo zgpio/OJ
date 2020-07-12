60c13ad5947290a9c0601875d1a34fe0aad87fb1
zgp <zgpio@qq.com>
2020-07-12 16:48:49 +0800

    lc128

---
+++ lc_main/lc128_最长连续序列.cc
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <unordered_set>
+#include <vector>
+using namespace std;
+
+// 最长连续整数序列
+int longestConsecutive(vector<int> &nums)
+{
+    unordered_set<int> num_set;
+    for (const int &num : nums) {
+        num_set.insert(num);
+    }
+
+    int longestStreak = 0;
+
+    // 时间复杂度为 O(n)
+    for (const int &num : num_set) {
+        if (!num_set.count(num - 1)) {
+            int j = num + 1;
+            int currentStreak = 1;
+
+            while (num_set.count(j)) {
+                j += 1;
+                currentStreak += 1;
+            }
+
+            longestStreak = max(longestStreak, currentStreak);
+        }
+    }
+
+    return longestStreak;
+}
+
+int main(int argc, char *argv[])
+{
+    {
+        vector<int> a{100, 4, 200, 1, 3, 2};
+        assert(longestConsecutive(a) == 4);
+    }
+    {
+        vector<int> a{0};
+        assert(longestConsecutive(a) == 1);
+    }
+    return 0;
+}
