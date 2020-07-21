835f435846eacb3f5b9369a4b338b8369a846161
Hack <zgpio@qq.com>
2020-07-21 10:01:52 +0800

    lc179

---
+++ lc_main/lc179_排列整数的顺序使之组成一个最大的整数.cc
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    string largestNumber(vector<int> &nums)
+    {
+        vector<string> vs;
+        for (auto i : nums) {
+            vs.push_back(to_string(i));
+        }
+        sort(vs.begin(), vs.end(), [](string &a, string &b) {
+            string s1 = a + b;
+            string s2 = b + a;
+            return s1 > s2;
+        });
+        string rv;
+        for (auto i : vs)
+            rv += i;
+        rv.erase(0, rv.find_first_not_of("0"));
+        if (rv.size() == 0) return "0";
+        return rv;
+    }
+};
+
+int main(int argc, char *argv[])
+{
+    Solution s;
+    {
+        vector<int> v{3, 30, 34, 5, 9};
+        assert(s.largestNumber(v) == "9534330");
+    }
+
+    return 0;
+}
