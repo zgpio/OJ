be39eeec77cd497f7ff8b038475b26cb0a116d8f
Hack <zgpio@qq.com>
2020-07-24 16:13:04 +0800

    genSpiralOrder

--- lc_main/lc54_螺旋矩阵.cpp
+++ lc_main/lc54_螺旋矩阵.cpp
@@ -8,15 +8,15 @@ vector<int> spiralOrder(vector<vector<int>> &matrix)
     int n = matrix[0].size();
     vector<int> ans;
 
-    // TODO: 应该还可以更加清晰
+    // m行n列, x索引行, y索引列
     for (int i = 0; i < n && i < m; ++i) {
-        int sx = i, sy = i;
-        int ex = m - i - 1, ey = n - i - 1;
+        int sx = i, ex = m - i - 1;
+        int sy = i, ey = n - i - 1;
         if (!(ex >= sx && ey >= sy)) break;
         if (ex == sx)
             for (int j = sy; j <= ey; ++j) ans.push_back(matrix[sx][j]);
         else if (sy == ey)
-            for (int j = sx; j <= ex; ++j) ans.push_back(matrix[j][ey]);
+            for (int j = sx; j <= ex; ++j) ans.push_back(matrix[j][sy]);
         else {
             // ────│
             // │   │
@@ -31,6 +31,33 @@ vector<int> spiralOrder(vector<vector<int>> &matrix)
     return ans;
 }
 
+vector<vector<int>> genSpiralOrder(int n, int m) // n行m列, y索引行, x索引列
+{
+    assert(m > 0 && n > 0);
+    vector<vector<int>> rv(n, vector<int>(m, 0));
+
+    int t = 0;
+    for (int i = 0; i < n && i < m; ++i) {
+        int sx = i, ex = m - i - 1;
+        int sy = i, ey = n - i - 1;
+        if (!(ex >= sx && ey >= sy)) break;
+        if (sx == ex)
+            for (int j = sy; j <= ey; ++j) rv[j][sx] = t++;
+        else if (sy == ey)
+            for (int j = sx; j <= ex; ++j) rv[sy][j] = t++;
+        else {
+            // ────│
+            // │   │
+            // │────
+            for (int j = sx; j < ex; ++j) rv[sy][j] = t++;
+            for (int j = sy; j < ey; ++j) rv[j][ex] = t++;
+            for (int j = ex; j > sx; --j) rv[ey][j] = t++;
+            for (int j = ey; j > sy; --j) rv[j][sx] = t++;
+        }
+    }
+
+    return rv;
+}
 
 int main()
 {
@@ -68,5 +95,7 @@ int main()
     for (auto c:cases) {
         assert(spiralOrder(c.first)==c.second);
     }
+    genSpiralOrder(5, 4);
+    genSpiralOrder(4, 5);
     return 0;
 }
