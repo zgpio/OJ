2b2a63bac3891691f0b1c85325d49e4eea91d8aa
Hack <zgpio@qq.com>
2020-07-24 10:22:05 +0800

    lc25

---
+++ lc_main/lc25_K个一组翻转链表.cc
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <lc/list.h>
+using namespace std;
+
+class Solution {
+public:
+    ListNode *reverseKGroup(ListNode *head, int k)
+    {
+        if (k == 1) return head;
+        ListNode hair(0);
+        hair.next = head;
+        ListNode *l = &hair;
+        int n = 0;
+        while (l->next) {
+            n++;
+            l = l->next;
+        }
+        n /= k;
+        int cnt = 1;
+        l = &hair;
+        // 将 head->next 指向的结点头插到子链表中
+        while (head && head->next && n)
+        {
+            auto *t = head->next;
+            head->next = head->next->next;
+            t->next = l->next;
+            l->next = t;
+            cnt++;
+            if (cnt == k) {
+                l = head;
+                head = head->next;
+                cnt = 1;
+                n--;
+            }
+        }
+        return hair.next;
+    }
+};
+
+int main(int argc, char *argv[])
+{
+    Solution s;
+    {
+        auto *l = s.reverseKGroup(buildList({1, 2, 3, 4, 5}), 2);
+        auto *gt = buildList({2, 1, 4, 3, 5});
+        printL(l);
+        equaList(l, gt);
+    }
+    {
+        auto *l = s.reverseKGroup(buildList({1, 2, 3, 4, 5}), 3);
+        auto *gt = buildList({3, 2, 1, 4, 5});
+        printL(l);
+        equaList(l, gt);
+    }
+
+    return 0;
+}
