86b515be2b740c6173a84c556f216988764fc915
Hack <zgpio@qq.com>
2020-07-25 10:17:36 +0800

    lc24

---
+++ lc_main/lc24. 两两交换链表中的节点.cc
@@ -0,0 +1,32 @@
+#include <lc/list.h>
+#include <iostream>
+using namespace std;
+
+class Solution
+{
+   public:
+    ListNode* swapPairs(ListNode* head)
+    {
+        ListNode hair(0);
+        hair.next = head;
+        ListNode* l = &hair;
+        while (l->next && l->next->next) {
+            ListNode* t = l->next->next;
+            l->next->next = t->next;
+            t->next = l->next;
+            l->next = t;
+            l = l->next->next;
+        }
+        return hair.next;
+    }
+};
+
+int main(int argc, char* argv[])
+{
+    Solution s;
+    ListNode* l = buildList({1, 2, 3, 4});
+    ListNode* gt = buildList({2, 1, 4, 3});
+    l = s.swapPairs(l);
+    equaList(l, gt);
+    return 0;
+}
