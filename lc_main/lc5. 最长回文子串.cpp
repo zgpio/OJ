382dc1d220aa41ab93651eba2b2e4f84f0979c86
Hack <zgpio@qq.com>
2020-07-26 19:04:02 +0800

    lc5

--- lc_main/lc5. 最长回文子串.cpp
+++ lc_main/lc5. 最长回文子串.cpp
@@ -1,14 +1,16 @@
 #include "lc/lc.h"
 using namespace std;
 
-class Solution {
+class Solution
+{
     bool dp[1000][1000];
 
-public:
+   public:
     string longestPalindrome(string s)
     {
         int n = s.size();
-        if (n < 1) return "";
+        if (n < 1)
+            return "";
         for (int i = 0; i < n; i++)
             dp[i][i] = true;
         int li = 0, lj = 0;
@@ -16,40 +18,35 @@ public:
             if (s[i] == s[i + 1]) {
                 dp[i][i + 1] = true;
                 li = i, lj = i + 1;
-            }
-            else
+            } else
                 dp[i][i + 1] = false;
         }
-        int b;
         // NOTE: 如果连续两次出现全false, 就可以提前终止.
-        for (b = 2; b < n; ++b) {
-
-            for (int i = 0, j = b; i < n - b && j < n; i++, j++) {
-                std::cout << i << "->" << j << "  ";
+        for (int b = 2; b < n; ++b) {  // b=j-i
+            for (int i = 0; i < n - b; i++) {
+                int j = i + b;
                 if (dp[i + 1][j - 1] && s[i] == s[j]) {
                     dp[i][j] = true;
                     li = i, lj = j;
-                }
-                else
+                } else
                     dp[i][j] = false;
             }
         }
 
-        std::cout << s.substr(li, lj - li + 1) << std::endl;
         return s.substr(li, lj - li + 1);
     }
 };
 
 int main()
 {
-    Solution sol;
-    assert(sol.longestPalindrome("babad") == "aba");
-    assert(sol.longestPalindrome("cbbd") == "bb");
-    assert(sol.longestPalindrome("") == "");
-    assert(sol.longestPalindrome("a") == "a");
-    assert(sol.longestPalindrome("bb") == "bb");
-    assert(sol.longestPalindrome("caba") == "aba");
-    assert(sol.longestPalindrome("abcba") == "abcba");
+    Solution s;
+    assert(s.longestPalindrome("babad") == "aba");
+    assert(s.longestPalindrome("cbbd") == "bb");
+    assert(s.longestPalindrome("") == "");
+    assert(s.longestPalindrome("a") == "a");
+    assert(s.longestPalindrome("bb") == "bb");
+    assert(s.longestPalindrome("caba") == "aba");
+    assert(s.longestPalindrome("abcba") == "abcba");
 
     return 0;
 }
