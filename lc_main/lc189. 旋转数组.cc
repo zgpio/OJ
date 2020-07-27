80b42656705b5d44818d31f7c1977273338af408
Hack <zgpio@qq.com>
2020-07-27 20:27:49 +0800

    lc189

---
+++ lc_main/lc189. 旋转数组.cc
@@ -0,0 +1,76 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution1
+{
+   public:
+    void rotate(vector<int>& nums, int k)
+    {
+        const int n = nums.size();
+        k = k % n;
+        int cnt = 0;
+        for (int start = 0; cnt < n; start++) {
+            int current = start;
+            int prev = nums[start];
+            do {
+                int next = (current + k) % n;
+                int temp = nums[next];
+                nums[next] = prev;
+                prev = temp;
+                current = next;
+                cnt++;
+            } while (start != current);
+        }
+    }
+};
+
+class Solution2
+{
+   public:
+    void rotate(vector<int>& nums, int k)
+    {
+        const int n = nums.size();
+        k %= n;
+        reverse(nums, 0, n - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, n - 1);
+    }
+
+   private:
+    void reverse(vector<int>& nums, int start, int end)
+    {
+        while (start < end) {
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
+        }
+    }
+};
+
+int main(int argc, char* argv[])
+{
+    Solution1 s1;
+    Solution1 s2;
+    {
+        vector<int> v = {1, 2, 3, 4, 5, 6, 7};
+        int k = 3;
+        s1.rotate(v, k);
+        for (auto i : v) {
+            std::cout << i << " ";
+        }
+        std::cout << std::endl;
+    }
+    {
+        vector<int> v = {1, 2, 3, 4, 5, 6, 7};
+        int k = 3;
+        s2.rotate(v, k);
+        for (auto i : v) {
+            std::cout << i << " ";
+        }
+        std::cout << std::endl;
+    }
+    return 0;
+}
