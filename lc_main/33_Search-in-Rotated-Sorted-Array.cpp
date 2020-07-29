0b8073e31539874dc4eb760f8e140c6c36e24964
Hack <zgpio@qq.com>
2020-07-29 22:20:15 +0800

    33

--- lc_main/33_Search-in-Rotated-Sorted-Array.cpp
+++ lc_main/33_Search-in-Rotated-Sorted-Array.cpp
@@ -1,41 +1,37 @@
 #include "lc/lc.h"
 using namespace std;
 
-class Solution {
-public:
+//    /│
+//   / │
+//  /  │
+// ────┼────
+//     │  /
+//     │ /
+//     │/
+class Solution
+{
+   public:
+    // 注意数组中不存在重复的元素
     int search(vector<int>& nums, int target)
     {
-        int l = 0, r = nums.size() - 1;
+        const int n = nums.size();
+        int l = 0, r = n - 1;
 
         while (l <= r) {
             int m = (l + r) / 2;
-            if (nums[m] == target) {
-                return m;
-            }
-
-            if (nums[l] > nums[r]) {
-                if (nums[m] > nums[r]) {
-                    if (nums[l] <= target && target <= nums[m]) {
-                        r = m - 1;
-                    }
-                    else if (target < nums[l] || target > nums[m]) {
-                        l = m + 1;
-                    }
-                }
-                else {
-                    if (nums[m] <= target && target <= nums[r]) {
-                        l = m + 1;
-                    }
-                    else if (target < nums[m] || target > nums[r]) {
-                        r = m - 1;
-                    }
-                }
-            }
-            else {
-                if (target < nums[m])
+            if (nums[m] == target) return m;
+            if (nums[0] <= nums[m]) {                         // 如果 m 落在前半段
+                if (nums[0] <= target && target < nums[m]) {  // 如果 target 落在前半段
                     r = m - 1;
-                else
+                } else {
                     l = m + 1;
+                }
+            } else {
+                if (nums[m] < target && target <= nums[n - 1]) {
+                    l = m + 1;
+                } else {
+                    r = m - 1;
+                }
             }
         }
         return -1;
@@ -53,6 +49,5 @@ int main()
     assert(sol.search(t1, 3) == -1);
     assert(sol.search(t2, 4) == -1);
     assert(sol.search(t3, 5) == 0);
-    std::cout << "end" << std::endl;
     return 0;
 }
