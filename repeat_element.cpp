b3d7c5b42f078ce9f7b1ae28d74969b6cacb8ecb
Hack <zgpio@qq.com>
2020-08-10 19:38:14 +0800

    tiny

--- repeat_element.cpp
+++ repeat_element.cpp
@@ -1,11 +1,10 @@
+#include <algorithm>
+#include <cmath>
 #include <cstdio>
 #include <cstdlib>
-#include <cmath>
-#include <iostream>
-#include <algorithm>
 #include <cstring>
+#include <iostream>
 #include <list>
-#include <time.h>
 using namespace std;
 
 int comp(const void *a, const void *b)
@@ -15,26 +14,26 @@ int comp(const void *a, const void *b)
 int main()
 {
 #ifndef ONLINE_JUDGE
-    freopen ("input.txt", "rt", stdin);
+    freopen("input.txt", "rt", stdin);
 #endif
     int n;
     int a[100];
-    while(scanf("%d", &n) != EOF) {
-        for(int i = 0; i < n; i++)
+    while (scanf("%d", &n) != EOF) {
+        for (int i = 0; i < n; i++)
             scanf("%d", &a[i]);
         qsort(a, n, sizeof(int), comp);
         int cnt = 0;
-        for (int i = 1; i < n; i++) {//去重
+        for (int i = 1; i < n; i++) {  //去重
             if (a[i] == a[i - 1]) {
                 a[i - 1] = -1;
-                cnt ++;//计数
+                cnt++;  //计数
             }
         }
         printf("%d\n", n - cnt);
         int first = 1;
-        for(int i = 0; i < n; i++)
+        for (int i = 0; i < n; i++)
             if (a[i] != -1) {
-                if(first) {
+                if (first) {
                     first = 0;
                     printf("%d", a[i]);
                 } else
