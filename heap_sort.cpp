c63a057f3bb23c329fd23fd92659ae63327f51c7
Hack <zgpio@qq.com>
2020-08-14 12:59:00 +0800

    heap_sort

--- heap_sort.cpp
+++ heap_sort.cpp
@@ -3,31 +3,32 @@ using namespace std;
 // 数据结构与算法分析 - C语言描述
 typedef int ElementType;
 #define LeftChild(i) (2 * (i) + 1)
-void PercDown(ElementType A[], int i, int N)
+// 下滤a[i]
+void PercDown(ElementType a[], int i, int n)
 {
-    int Child;
-    ElementType Tmp;
+    int child;
+    ElementType tmp;
 
-    for (Tmp = A[i]; LeftChild(i) < N; i = Child) {
-        Child = LeftChild(i);
-        if (Child != N - 1 && A[Child + 1] > A[Child]) Child++;
-        if (Tmp < A[Child])
-            A[i] = A[Child];
+    for (tmp = a[i]; LeftChild(i) < n; i = child) {
+        child = LeftChild(i);
+        if (child != n - 1 && a[child + 1] > a[child]) child++;
+        if (tmp < a[child])
+            a[i] = a[child];
         else
             break;
     }
-    A[i] = Tmp;
+    a[i] = tmp;
 }
 
-void Heapsort(ElementType A[], int N)
+void Heapsort(ElementType a[], int n)
 {
     int i;
 
-    for (i = N / 2; i >= 0; i--) /* BuildHeap */
-        PercDown(A, i, N);
-    for (i = N - 1; i > 0; i--) {
-        swap(A[0], A[i]); /* DeleteMax */
-        PercDown(A, 0, i);
+    for (i = n / 2; i >= 0; i--) // BuildHeap
+        PercDown(a, i, n);
+    for (i = n - 1; i > 0; i--) {
+        swap(a[0], a[i]); // DeleteMax
+        PercDown(a, 0, i);
     }
 }
 
