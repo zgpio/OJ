987070a5f6ad810780af62f37b86976f2df6287c
B_zgp <zgpio@qq.com>
2020-09-03 10:27:35 +0800

    fix

--- lc_main/tree/求二叉树第k层的节点数.cc
+++ lc_main/tree/求二叉树第k层的节点数.cc
@@ -5,8 +5,7 @@ using namespace std;
 int klevel_cnt(TreeNode *t, int k)
 {
     if (t) {
-        if (k == 0)
-            return 1;
+        if (k == 0) return 1;
         int l = klevel_cnt(t->left, k - 1);
         int r = klevel_cnt(t->right, k - 1);
         return l + r;
@@ -19,7 +18,9 @@ int main(int argc, char *argv[])
     //    1
     //  2   3
     // 4 5 6 7
-    vector<tuple<string, int, int>> cases = {{string("[1, 2, 3, 4, 5, 6, 7]"), 2, 4}, {string("[]"), 2, 0}};
+    vector<tuple<string, int, int>> cases = {
+        tuple<string, int, int>{"[1, 2, 3, 4, 5, 6, 7]", 2, 4},
+        tuple<string, int, int>{"[]", 2, 0}};
     for (auto c : cases) {
         TreeNode *t = constructT(std::get<0>(c));
         inorder(t);
